6ddcb6fd02683ae9323bdbf2940f7c89764b72c6
Shokhruz <forpk432@gmail.com>
2024-02-14 13:02:36 +0500

    added_speed_insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Hanken_Grotesk } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 import Header from "@/components/header";
 
